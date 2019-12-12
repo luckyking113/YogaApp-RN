6c6ce7e5ba476d0c2c831faa0d319a1b34e95266
Love <love@Loves-iMac.local>
2019-12-12 10:54:43 +0800

    add search member screen

--- App/Containers/MemberScreen/MemberScreen.js
+++ App/Containers/MemberScreen/MemberScreen.js
@@ -1,14 +1,55 @@
-import React from 'react'
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
+import SearchInput, { createFilter } from 'react-native-search-filter';
+import emails from './emails';
+const KEYS_TO_FILTERS = ['user.name', 'subject'];
+
 import styles from './MemberScreenStyles';
 
 export default class MemberScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ''
+    }
+  }
+
+  searchUpdated(term) {
+    this.setState({ searchTerm: term })
+  }
   render() {
+    const filteredEmails = emails.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
     return (
       <View style={styles.container}>
-        {/* You will probably want to insert your logo here */}
-        <Text>MemberScreen</Text>
+        <View style={styles.searchContainer}>
+          <View style={{width:'65%'}}>
+            <SearchInput 
+              onChangeText={(term) => { this.searchUpdated(term) }} 
+              style={styles.searchInput}
+              placeholder="Type a message to search"
+            />
+          </View>          
+          <View style={{paddingStart:'5%',width:'35%', flexDirection:'row', justifyContent:'space-between'}}>
+            <Text>Hello</Text>
+            <Text>Hello</Text>
+          </View>    
+        </View>        
+        <ScrollView style={styles.listViewContainer}>
+          {filteredEmails.map(email => {
+            return (
+              <TouchableOpacity onPress={()=>alert(email.user.name)} key={email.id} style={styles.emailItem}>
+                <View>
+                  <Image source={require('../../Assets/Images/homeIcon.png')} style={{width:50, height:50, marginRight:10}} />
+                </View>
+                <View>
+                  <Text>{email.user.name}</Text>
+                  <Text style={styles.emailSubject}>{email.subject}</Text>
+                </View>
+              </TouchableOpacity>
+            )
+          })}
+        </ScrollView>
       </View>
-    )
+    );
   }
 }
--- App/Containers/MemberScreen/MemberScreenStyles.js
+++ App/Containers/MemberScreen/MemberScreenStyles.js
@@ -6,6 +6,37 @@ export default StyleSheet.create({
   container: {
     ...ApplicationStyles.screen.container,
     display: 'flex',    
-    alignItems: 'center',
+    alignItems: 'flex-start',    
   },
+  searchContainer:{
+    width:'100%',
+    height:60,
+    paddingVertical:5,
+    paddingHorizontal:20,
+    flexDirection:'row',
+    alignItems:'center',
+  },
+  emailItem:{
+    borderBottomWidth: 0.5,
+    borderColor: 'rgba(0,0,0,0.1)',
+    padding: 10,
+    flexDirection:'row',    
+    alignItems:'center',    
+  },
+  emailSubject: {
+    color: 'rgba(0,0,0,0.5)'
+  },
+  searchInput:{
+    padding: 10,
+    // borderColor: '#CCC',
+    // borderWidth: 1,
+    backgroundColor:'white',
+    width:'100%',
+    borderRadius:5
+  },
+  listViewContainer:{
+    width:'100%',
+    padding:10,
+    backgroundColor:'white'
+  }
 })
